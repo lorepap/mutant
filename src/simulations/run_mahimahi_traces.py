f3876a1a150202f9fd3675250314cea17cf89cad
lorepap <p.lore96@gmail.com>
2023-03-22 14:41:04 -0500

    training with mahimahi traces

--- src/simulations/run_mahimahi_traces.py
+++ src/simulations/run_mahimahi_traces.py
@@ -1,5 +1,5 @@
 """
-Generate experiments to test all traces.
+Generate experiments to train MahiMahi traces.
 Traces located in traces/
 Look at models.yml to see available models for MAB
 
@@ -20,10 +20,9 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 
 from ml.helper import context, utils
 
-SCRIPT_FILENAME = os.path.join(context.entry_dir, "tests", "traces_tests.sh")  # name of bash script to generate
 TRAINING_FILENAME = os.path.join(context.ml_dir, "train.py")
 
-def generate_experiments(model):
+def run_experiments(args):
     ip = utils.get_private_ip()
     
     
@@ -33,35 +32,30 @@ def generate_experiments(model):
     # Select only mahimahi traces for training
     trace_names = ["att.lte.driving", "att.lte.driving.2016", "tm.lte.driving", "tm.lte.short", "tm.umts.driving", "vz.evdo.driving", "vz.lte.driving", "vz.lte.short"]
     trace_data = {name: data for name, data in yaml_data["traces"].items() if name in trace_names}
+    test = trace_data.keys()
+    for i, trace_name in enumerate(trace_data.keys()):
 
-
-    # generate bash script to execute experiments for each trace
-    os.chmod(SCRIPT_FILENAME, 0o777)
-
-    with open(SCRIPT_FILENAME, "w") as script_file:
-
-        for trace_name, trace_info in trace_data["traces"].items():
-
-            trace_path = trace_name
-            # generate command to execute for this trace
-            command = f"python3 {TRAINING_FILENAME} -m {model} -t {trace_path} -x {ip} -e 86400 -rt 0"
-            # write command to script file
-            script_file.write(command + "\n")
+        trace_path = trace_name
+        print(trace_path)
+        # Train from scratch on the first trace, then retrain the same model over the others
+        if args.retrain == 1:
+            retrain = 1 if i > 0 else 0
+        else:
+            retrain = 1
+        # generate command to execute for this trace
+        command = f"python3 {TRAINING_FILENAME} -m {args.model} -t {trace_path} -x {ip} -e 86400 -rt {retrain}"
+        print("Executing", command)
+        # execute each command and wait for it to finish
+        try:
+            subprocess.check_call(command, shell=True, stderr=sys.stderr, stdout=sys.stdout, bufsize=1)
+        
+        except subprocess.CalledProcessError as e:
+            print(f"Error running command '{command}': {e}")
     
-    
-def run_experiments():
-    # read commands from script file
-    with open(SCRIPT_FILENAME, "r") as script_file:
-        commands = script_file.read().splitlines()
-
-    # execute each command and wait for it to finish
-    for command in commands:
-        subprocess.run(command, shell=True, check=True)
     
 if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("--model", "-m", help="MAB policy to train")
     parser.add_argument("--retrain", "-r", help="True: retrain from scratch")
     args = parser.parse_args()
-    generate_experiments(model=args.model)
-    run_experiments()
+    run_experiments(args)
